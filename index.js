471955ebd427f9e7a264b46d677c5e339bd03863
Daniyal1432 <108730783+Daniyal1432@users.noreply.github.com>
2024-07-24 09:49:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 
-const cors = require("cors");
-const path = require("path");
+// const cors = require("cors");
+// const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 
 const connectDB = require("./dbconnection");
 
 connectDB();
+
+const productModel =require('./models/Product')
+const Catalog =require('./models/Catalog')
 
 // const catalogRoutes = require("./routes/catalog");
 // const productRoutes = require("./routes/product");
@@ -25,6 +28,39 @@ connectDB();
 // // Serve static files from the 'uploadss' folder
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // console.log('www22333')
+
+
+app.post("/get_Product", async (req, res) => {
+  const { filter, search } = req.body;
+
+  try {
+    const query = {};
+
+    if (filter && filter.length > 0) {
+      query.catalog_type = { $in: filter };
+    }
+
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    const products = await ProductModel.find(query).populate("catalog_type");
+    res.json({
+      success: true,
+      message: "Products retrieved successfully",
+      data: products,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve products",
+      error: err.message,
+    });
+  }
+});
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
